398a1aaa2940ff57f2444fac27b2078946f884ed
capable-publisher-7225 <marcusplenty@gmail.com>
2018-11-19 09:05:35 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,15 +4,15 @@ def run_guessing_game
   while true 
     computer = rand(1..6)
     input = gets.chomp()
-    
-    if input == computer
-      puts "You guessed the correct number!"
-    end
-    else 
-      puts "The computer guessed #{computer}."
-    elsif input == "exit"
+    if input == "exit"
       puts "Goodbye!"
       break
     end
+    if input == computer
+      puts "You guessed the correct number!"
+    else 
+      puts "The computer guessed #{computer}."
+    end
+    
   end
 end 
